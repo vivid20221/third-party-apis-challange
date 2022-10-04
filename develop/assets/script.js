57d8ee70b9dcce71fed903121c278b676626f3fe
vivid20221 <julainj9011@gmail.com>
2022-10-03 20:15:19 -0500

    new

---
+++ develop/assets/script.js
@@ -0,0 +1,14 @@
+// var textarea  
+// var saveBtn 
+
+$(".saveBtn").on("click", function() {
+    var time = $(this).parent().parent()
+    var descripton = $(this).parent()
+
+
+}); 
+ 
+localStorage.setItem(TIME, APPT DESC);
+var setDesc = localStorage.getItem(9);
+
+$(‘.time-block).val(localStorage.getItem())
